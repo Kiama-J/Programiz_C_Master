ec5743f6d1f7acf0e2fe76c710207b6e9f239d92
Julius Ndirangu <alvinjuliuskiamar@gmail.com>
2023-05-08 17:25:37 +0300

    Changing variable value

---
+++ Introduction/Variables/code3.c
@@ -0,0 +1,19 @@
+//Create an integer variable number with value 31.
+//Print the variable.
+//Change the value of the variable to 99.
+//Print the variable again with a space before it.
+
+#include <stdio.h>
+
+int main() 
+{
+    
+    int number = 31;
+    printf("%d ", number);
+    
+    number = 99;
+    printf("%d", number);
+  
+    
+    return 0;
+}
