14c939b1471b35c11bbc4baa4be644e3282134bb
Julius Ndirangu <alvinjuliuskiamar@gmail.com>
2023-05-09 09:06:49 +0300

    Arithmetics

---
+++ Introduction/Arithmetic_Operator/code7.c
@@ -0,0 +1,17 @@
+//Create an integer variable number1 with value 34.
+//Create a double variable number2 with value 54.9.
+//Add number1 and number2 and assign the result to a double variable result.
+//Print result.
+
+#include <stdio.h>
+
+int main() 
+{
+        int number1 = 34;
+        double number2 = 54.9;
+        double result = number1 + number2;
+        
+        printf("%lf", result);
+    
+    return 0;
+}
