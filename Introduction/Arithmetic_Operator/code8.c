a292f69500ab924a19b20e5b0eb677a40deed3ba
Julius Ndirangu <alvinjuliuskiamar@gmail.com>
2023-05-09 09:16:43 +0300

    Type conversion

---
+++ Introduction/Arithmetic_Operator/code8.c
@@ -0,0 +1,20 @@
+//Declare a double variable number and assign 18.7 to it.
+//Declare an int variable result.
+//Explicitly convert number to int and add 5 to it.
+//Store the sum in result.
+//Print result.
+
+#include <stdio.h>
+
+int main() 
+{
+    double number = 18.7;
+    int result;
+    
+    result = (int) number + 5;
+    
+    printf("%d", result);
+   
+
+    return 0;
+}
