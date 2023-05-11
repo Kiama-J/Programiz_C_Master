2d528803de4c1e8d238d9cc12c9d2a66db75ace7
Julius Ndirangu <alvinjuliuskiamar@gmail.com>
2023-05-11 16:00:32 +0300

    code25

---
+++ Flow_Control/canVote.c
@@ -0,0 +1,18 @@
+//Create a program to check whether a person can vote or not depending on his/her age.
+//Get integer input from the user and store it in the age variable.
+//If age is 18 or above, print The person can vote. If not, don't print anything.
+
+#include <stdio.h>
+
+int main() 
+{
+    int age;
+    
+    scanf("%d", &age);
+    
+    if (age >= 18){
+        printf("The person can vote.");
+    }
+   
+    return 0;
+}
