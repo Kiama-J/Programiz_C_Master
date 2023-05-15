2eccda7768add0917fb7e08cf5e6e2d729d011f5
Julius Ndirangu <alvinjuliuskiamar@gmail.com>
2023-05-15 14:02:51 +0300

    Code32

---
+++ Flow_Control/forLoop.c
@@ -0,0 +1,15 @@
+//Create a program to print numbers from 5 to 1 using a loop.
+//Initialize a variable i with value 5.
+//Inside the for loop, print the value of i.
+//Decrease the value of i by 1 in each iteration of the loop.
+
+#include <stdio.h>
+
+int main() 
+{
+    for(int i = 5; i > 0; i--){
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
