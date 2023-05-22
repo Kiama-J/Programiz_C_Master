9ee5f6fc2b920adc62d59ba38db4f5af6f4159e5
Julius Ndirangu <alvinjuliuskiamar@gmail.com>
2023-05-22 13:49:31 +0300

    Code48

---
+++ Flow_Control/C_Flow_Control_Challenges/challengeH.c
@@ -0,0 +1,16 @@
+//Create a program to print a sentence 3 times using a loop.
+//Run a for loop from i = 0 to i < 3.
+//Inside the loop, print C Programming is fun.
+
+#include <stdio.h>
+
+int main() 
+{
+    int i;
+    
+    for(i = 0; i < 3; i++){
+        printf("C Programming is fun\n");
+    }
+    
+      return 0;
+}
