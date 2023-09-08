0f2c535a0f3cfa3fa452a05ce551be0ba2222063
Julius Ndirangu <alvinjuliuskiamar@gmail.com>
2023-09-08 16:43:45 +0300

    String1

---
+++ Strings/String_introduction/characterAccess.c
@@ -0,0 +1,15 @@
+//Access a Character
+
+//Create a program to access a character from the string
+//Assign the "Snowpiercer" string to a variable named movie.
+//Print the fourth character from the string.
+
+#include <stdio.h>
+
+int main() {
+    char movie[] = "Snowpiercer";
+    printf("%c", movie[3]);
+    
+    return 0;
+    
+}
